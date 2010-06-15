81bbd00593c4e88d27b5fb3d178d29440c65435c
adrian.mihalcea@gmail.com <adrian.mihalcea@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2010-06-15 10:04:16 +0000

    makes sure input and error streams of the process are consumed to avoid blocking/deadlock issues
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@35 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- goclipse-n/src/com/googlecode/goclipse/builder/ExternalCommand.java
+++ goclipse-n/src/com/googlecode/goclipse/builder/ExternalCommand.java
@@ -1,6 +1,7 @@
 package com.googlecode.goclipse.builder;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -63,18 +64,35 @@ public class ExternalCommand {
 			SysUtils.debug(pBuilder.directory() + " executing: " +  args);
 			
 			Process p = pBuilder.start();
+			InputStream is = p.getInputStream();
 			if (resultsFilter != null) {
 				resultsFilter.clear();
-				InputStream is = p.getInputStream();
-				resultsFilter.process(is);
-				is.close();
+				resultsFilter.process(is);//the filter will use the stream
 			}
+			consume(is);
+			is.close();
+			//should allow error stream to be filtered
+			InputStream es = p.getErrorStream();
+			consume(es);
+			es.close();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 			rez = e.getLocalizedMessage();
 		}
 		return rez;
+	}
+	
+	private void consume(InputStream is) {
+		//go through stream up to the end
+		byte[] buf = new byte[256];
+		try {
+			while(is.read(buf, 0, buf.length)>0) {
+				//just consume data
+			}
+		}catch(IOException e) {
+			//ignore
+		}
 	}
 
 }
