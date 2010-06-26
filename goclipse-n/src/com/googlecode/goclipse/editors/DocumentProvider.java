cc8501cea1b8f55bd0f40068260bce3ea986486e
adrian.mihalcea@gmail.com <adrian.mihalcea@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2010-06-26 17:35:14 +0000

    forces UTF-8 on GoEditor. Now hello world from the tutorial works as it should.
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@49 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- goclipse-n/src/com/googlecode/goclipse/editors/DocumentProvider.java
+++ goclipse-n/src/com/googlecode/goclipse/editors/DocumentProvider.java
@@ -4,6 +4,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
 
 /**
@@ -11,11 +12,18 @@ import org.eclipse.ui.editors.text.FileDocumentProvider;
  * @author steel
  */
 public class DocumentProvider extends FileDocumentProvider {
-
+	
+	private static final String CHARSET="UTF-8";
+	
    /**
     * 
     */
 	protected IDocument createDocument(Object element) throws CoreException {
+		IFileEditorInput fei = (IFileEditorInput)element;
+		if (!fei.getFile().getCharset().equalsIgnoreCase(CHARSET)) {
+			//this forces a build!
+			fei.getFile().setCharset(CHARSET, null);	
+		}
 		IDocument document = super.createDocument(element);
 		if (document != null) {
 			IDocumentPartitioner partitioner =
@@ -31,4 +39,5 @@ public class DocumentProvider extends FileDocumentProvider {
 		}
 		return document;
 	}
+	
 }
