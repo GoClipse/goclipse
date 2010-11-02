46781918987bc9379cc8ccc032293b0f8abdfa42
graham.miller <graham.miller@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2010-11-02 15:16:31 +0000

    first checkin of gocode-based autocomplete code
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@66 1731708e-d1f7-11de-a1df-87ee4daaac7b

---
+++ org.googlecode.goclipse.gocode/src/com/googlecode/goclipse/gocode/ContentAssistProcessor.java
@@ -0,0 +1,103 @@
+package com.googlecode.goclipse.gocode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ContextInformation;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.contentassist.IContextInformationValidator;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPathEditorInput;
+
+import com.googlecode.goclipse.editors.CompletionProcessor;
+
+public class ContentAssistProcessor implements IContentAssistProcessor {
+
+	GoCodeClient client = new GoCodeClient();
+	private Image go = com.googlecode.goclipse.Activator.getImageDescriptor("icons/go-icon16.png").createImage();
+
+	
+	public ContentAssistProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
+		IPath path = null;
+		try {
+			IEditorPart editor = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+			IEditorInput input = editor.getEditorInput();
+			if (input instanceof IPathEditorInput) {
+				IPathEditorInput pathInput = (IPathEditorInput) input;
+				path = pathInput.getPath();
+			}
+		} catch (NullPointerException npe) {
+		}
+        ArrayList<ICompletionProposal> results = new ArrayList<ICompletionProposal>();
+		if (path != null){
+			String fileName = path.toOSString();
+			IDocument document = viewer.getDocument();
+			List<String> completions = client.getCompletions(fileName, document.get(), offset);
+	
+			if (completions != null) {
+				for (String string : completions) {
+					String prefix = "";
+			         prefix = CompletionProcessor.lastWord(document, offset);
+
+					int firstComma = string.indexOf(",,");
+					int secondComma = string.indexOf(",,", firstComma+2);
+					if (firstComma != -1 && secondComma != -1) {
+						String type = string.substring(0, firstComma);
+						String identifier = string.substring(firstComma+2, secondComma);
+						String spec = string.substring(secondComma+2);
+						
+						String descriptiveString = identifier+" : "+spec;
+						IContextInformation info = new ContextInformation(descriptiveString, "");
+						//MessageFormat.format(JavaEditorMessages.getString("CompletionProcessor.Proposal.ContextInfo.pattern"), new Object[] { fgProposals[i] })); //$NON-NLS-1$
+						results.add(new CompletionProposal(identifier.substring(prefix.length()), offset, 0, identifier.length() - prefix.length(), go, descriptiveString, info, ""));
+					}
+				}
+			}
+		}
+		return results.toArray(new ICompletionProposal[] {});
+	}
+
+	@Override
+	public IContextInformation[] computeContextInformation(ITextViewer viewer,
+			int offset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char[] getCompletionProposalAutoActivationCharacters() {
+		return new char[] {'.'};
+	}
+
+	@Override
+	public char[] getContextInformationAutoActivationCharacters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		// TODO Auto-generated method stub
+		return client.getError();
+	}
+
+	@Override
+	public IContextInformationValidator getContextInformationValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ org.googlecode.goclipse.gocode/src/com/googlecode/goclipse/gocode/GoCodeClient.java
@@ -0,0 +1,59 @@
+package com.googlecode.goclipse.gocode;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.Status;
+
+import com.googlecode.goclipse.Activator;
+import com.googlecode.goclipse.SysUtils;
+import com.googlecode.goclipse.builder.ExternalCommand;
+import com.googlecode.goclipse.builder.GoConstants;
+import com.googlecode.goclipse.builder.ProcessOStreamFilter;
+import com.googlecode.goclipse.builder.StreamAsLines;
+
+public class GoCodeClient {
+
+	private String error;
+
+	public GoCodeClient() {
+	}
+	
+	public List<String> getCompletions(String fileName, final String bufferText, int offset) {
+		ExternalCommand command = new ExternalCommand("/home/gmiller/go/bin/gocode");
+		StreamAsLines output = new StreamAsLines();
+		command.setResultsFilter(output);
+		command.setInputFilter(new ProcessOStreamFilter() {
+			@Override
+			public void setStream(OutputStream outputStream) {
+				OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+				try {
+					osw.append(bufferText);
+					osw.flush();
+					outputStream.close();
+				} catch (IOException e) {
+					// do nothing
+				}
+			}
+		});
+
+		List<String> parameters = new LinkedList<String>();
+		parameters.add("-f=csv");
+		parameters.add("autocomplete");
+		parameters.add(fileName);
+		parameters.add(""+offset);
+		error = command.execute(parameters, true);
+		if (error != null) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.PLUGIN_ID, error));
+		}
+		return output.getLines();
+	}
+
+	public String getError() {
+		return error;
+	}
+
+}
