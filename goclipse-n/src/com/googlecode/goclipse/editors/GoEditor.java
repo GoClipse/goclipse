8be8dda570850a3b7506f9aa15f72209b194f88e
adrian.mihalcea@gmail.com <adrian.mihalcea@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2010-11-14 01:27:19 +0000

    enable bracket matching in Go editor, go to matching bracket with shortcut M1+M2+P (CTRL+SHIFT+P on windows)
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@83 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- goclipse-n/src/com/googlecode/goclipse/editors/GoEditor.java
+++ goclipse-n/src/com/googlecode/goclipse/editors/GoEditor.java
@@ -1,15 +1,23 @@
 package com.googlecode.goclipse.editors;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IDocumentExtension3;
+import org.eclipse.jface.text.source.DefaultCharacterPairMatcher;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.texteditor.SourceViewerDecorationSupport;
 
 import com.googlecode.goclipse.Activator;
 
 public class GoEditor extends TextEditor {
+	
+	public final static String EDITOR_MATCHING_BRACKETS = "matchingBrackets";
+	public final static String EDITOR_MATCHING_BRACKETS_COLOR= "matchingBracketsColor";
 
 	private ColorManager colorManager;
 	private IPropertyChangeListener changeListener;
+	private DefaultCharacterPairMatcher matcher;
 	
 	
 	public GoEditor() {
@@ -18,6 +26,7 @@ public class GoEditor extends TextEditor {
 		setDocumentProvider(new DocumentProvider());
 		final GoEditor instance = this;
 		
+		setKeyBindingScopes(new String[] {"com.googlecode.goclipse.editor"});
 		
 		changeListener = new IPropertyChangeListener() {
 
@@ -34,8 +43,30 @@ public class GoEditor extends TextEditor {
 		
 		Activator.getDefault().getPreferenceStore().addPropertyChangeListener(changeListener);
 	}
+	 
+	@Override
+	protected void configureSourceViewerDecorationSupport (SourceViewerDecorationSupport support) {
+		super.configureSourceViewerDecorationSupport(support);		
+	 
+		char[] matchChars = {'(', ')', '[', ']', '{', '}'}; //which brackets to match		
+		matcher = new DefaultCharacterPairMatcher(matchChars ,
+				IDocumentExtension3.DEFAULT_PARTITIONING);
+		support.setCharacterPairMatcher(matcher);
+		support.setMatchingCharacterPainterPreferenceKeys(EDITOR_MATCHING_BRACKETS,EDITOR_MATCHING_BRACKETS_COLOR);
+		
+	 
+		//Enable bracket highlighting in the preference store
+		IPreferenceStore store = getPreferenceStore();
+		store.setDefault(EDITOR_MATCHING_BRACKETS, true);
+		store.setDefault(EDITOR_MATCHING_BRACKETS_COLOR, "128,128,128");
+	}
 
 	
+	
+	public DefaultCharacterPairMatcher getPairMatcher() {
+		return matcher;
+	}
+
 	public void dispose() {
 		if (colorManager != null) {
 			colorManager.dispose();
---
+++ goclipse-n/src/com/googlecode/goclipse/editors/PairMatcher.java
@@ -0,0 +1,73 @@
+package com.googlecode.goclipse.editors;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.source.DefaultCharacterPairMatcher;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class PairMatcher extends Action implements IWorkbenchWindowActionDelegate{
+	private GoEditor editor;
+	private ISelection selection;
+
+	@Override
+	public void run() {
+		super.run();
+	}
+
+	@Override
+	public void run(IAction action) {
+		if (editor != null && selection != null && selection instanceof TextSelection) {
+			DefaultCharacterPairMatcher matcher = editor.getPairMatcher();
+			IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+			Object obj = editor.getAdapter(Control.class);
+			if (obj != null && obj instanceof StyledText) {
+				StyledText st = (StyledText)obj;
+				int startOffset = st.getCaretOffset();
+				IRegion r = matcher.match(doc, startOffset);
+				if (r != null) {
+					int ro = r.getOffset();
+					int low = ro + 1;
+					int high = ro + r.getLength();
+					int pairOffset = low == startOffset?high:low;
+				
+					st.setCaretOffset(pairOffset);
+				}
+			}
+			
+		}
+		selection = null;
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;		
+	}
+
+	@Override
+	public void dispose() {
+		selection = null;
+		editor = null;
+		
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		System.out.println(window);
+		IEditorPart editorPart = window.getActivePage().getActiveEditor();
+		if (editorPart instanceof GoEditor) {
+			editor = (GoEditor)editorPart;
+		} else {
+			editor = null;
+		}
+	}
+	
+	
+}
