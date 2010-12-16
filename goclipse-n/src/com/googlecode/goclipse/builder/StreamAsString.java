b6b867f15d796a48c45e489320dc2b5c82ea87eb
graham.miller@gmail.com <graham.miller@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2010-12-16 16:33:37 +0000

    Added the ability to run gofmt on the current buffer, including a source menu associated with the goeditor.
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@108 1731708e-d1f7-11de-a1df-87ee4daaac7b

---
+++ goclipse-n/src/com/googlecode/goclipse/builder/StreamAsString.java
@@ -0,0 +1,37 @@
+package com.googlecode.goclipse.builder;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.googlecode.goclipse.SysUtils;
+
+public class StreamAsString implements ProcessIStreamFilter {
+
+	private StringBuilder streamContent;
+	@Override
+	public void process(InputStream iStream) {
+		try {
+			clear();
+			InputStreamReader isr = new InputStreamReader(iStream);
+		    BufferedReader br = new BufferedReader(isr);
+		    char [] cbuf = new char[4096];
+		    int n;
+		    while ((n=br.read(cbuf))>0) {
+	        	streamContent.append(cbuf, 0, n);
+	        }
+		}catch(Exception e) {
+			SysUtils.debug(e);
+		}
+	}
+	public String getString() {
+		return streamContent == null ? "" : streamContent.toString();
+	}
+	
+	public void clear() {
+		streamContent = new StringBuilder();
+	}
+
+}
---
+++ goclipse-n/src/com/googlecode/goclipse/editors/GofmtActionDelegate.java
@@ -0,0 +1,95 @@
+package com.googlecode.goclipse.editors;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.internal.Workbench;
+
+import com.googlecode.goclipse.Activator;
+import com.googlecode.goclipse.Environment;
+import com.googlecode.goclipse.SysUtils;
+import com.googlecode.goclipse.builder.ExternalCommand;
+import com.googlecode.goclipse.builder.GoConstants;
+import com.googlecode.goclipse.builder.ProcessOStreamFilter;
+import com.googlecode.goclipse.builder.StreamAsString;
+import com.googlecode.goclipse.preferences.PreferenceConstants;
+
+public class GofmtActionDelegate implements IEditorActionDelegate {
+    private IWorkbenchWindow window;
+	private IEditorPart editorPart;
+
+    public GofmtActionDelegate() {
+	}
+
+	@Override
+	public void run(IAction action) {
+		GoEditor editor;
+		if (editorPart instanceof GoEditor) {
+			editor = (GoEditor)editorPart;
+		} else {
+			editor = null;
+		}
+		if (editor != null) {
+			Object obj = editor.getAdapter(Control.class);
+			if (obj != null && obj instanceof StyledText) {
+				StyledText st = (StyledText)obj;
+				final String currentContent = st.getText();
+
+				IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+				String gofmtPath = preferenceStore.getString(PreferenceConstants.FORMATTER_PATH);
+				final ExternalCommand gofmtCmd = new ExternalCommand(gofmtPath);
+				gofmtCmd.setEnvironment(GoConstants.environment());
+
+				gofmtCmd.setInputFilter(new ProcessOStreamFilter() {
+					@Override
+					public void setStream(OutputStream outputStream) {
+						OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+						try {
+							osw.append(currentContent);
+							osw.flush();
+							outputStream.close();
+						} catch (IOException e) {
+							// do nothing
+						}
+					}
+				});
+
+				StreamAsString output = new StreamAsString();
+				gofmtCmd.setResultsFilter(output);
+	
+				String result = gofmtCmd.execute(new ArrayList<String>(), true);
+				
+				if (result != null) {
+					SysUtils.warning("gofmt failed: "+result);
+				} else {
+					String formattedText = output.getString();
+					if (!formattedText.equals(currentContent)) {
+						st.setText(formattedText);
+					}
+				}
+			}			
+		}
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+
+	}
+
+	@Override
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		editorPart = targetEditor;
+	}
+
+}
