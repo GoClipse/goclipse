82a43f1f8bd53accf5ed52acdf401bde9ea672ca
devoncarew@gmail.com <devoncarew@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2011-06-03 04:05:52 +0000

    General cleanup of the Go perspective. Added view shortcuts for common views, added wizard shortcuts. Added placeholders for common views in the perspective layout.
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@147 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- goclipse-n/src/com/googlecode/goclipse/perspectives/GoPerspective.java
+++ goclipse-n/src/com/googlecode/goclipse/perspectives/GoPerspective.java
@@ -10,25 +10,24 @@
  *******************************************************************************/
 package com.googlecode.goclipse.perspectives;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
 
+// WARNING:  Reset the perspective when testing this class!!!  Otherwise the 
+//           changes will not display.
 
 /**
- *  This class is meant to serve as an example for how various contributions 
- *  are made to a perspective. Note that some of the extension point id's are
- *  referred to as API constants while others are hardcoded and may be subject 
- *  to change. 
- *  
- *  WARNING:  Reset the perspective when testing this class!!!  Otherwise the 
- *            changes will not display.
+ * The Go perspective definition.
  */
 public class GoPerspective implements IPerspectiveFactory {
 
 	public static final String ID = "com.googlecode.goclipse.perspectives.GoPerspective";
+	
 	private IPageLayout factory;
 
 	public GoPerspective() {
@@ -37,13 +36,30 @@ public class GoPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout factory) {
 		this.factory = factory;
+		
 		addViews();
+		
 		addActionSets();
+		
+		addPerspectiveShortcuts();
+		addNewWizardShortcuts();
+		addViewShortcuts();
 	}
 
+	/**
+	 * Creates the overall folder layout.
+	 */
+	@SuppressWarnings("deprecation")
 	private void addViews() {
-		// Creates the overall folder layout. 
-		// Note that each new Folder uses a percentage of the remaining EditorArea.
+		IFolderLayout topLeft =
+			factory.createFolder(
+				"topLeft", //NON-NLS-1
+				IPageLayout.LEFT,
+				0.25f,
+				factory.getEditorArea());
+		topLeft.addView("org.eclipse.ui.navigator.ProjectExplorer"); //NON-NLS-1
+		topLeft.addPlaceholder(IPageLayout.ID_RES_NAV);
+		topLeft.addPlaceholder(JavaUI.ID_PACKAGES);
 		
 		IFolderLayout bottom =
 			factory.createFolder(
@@ -54,17 +70,20 @@ public class GoPerspective implements IPerspectiveFactory {
 		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
 		bottom.addView("org.eclipse.team.ui.GenericHistoryView"); //NON-NLS-1
 		bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
-
-		IFolderLayout topLeft =
-			factory.createFolder(
-				"topLeft", //NON-NLS-1
-				IPageLayout.LEFT,
-				0.25f,
-				factory.getEditorArea());
-		
-		topLeft.addView("org.eclipse.ui.navigator.ProjectExplorer"); //NON-NLS-1
-		topLeft.addView(IPageLayout.ID_RES_NAV);
+		bottom.addPlaceholder("org.eclipse.search.ui.views.SearchView");
+		bottom.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+		bottom.addPlaceholder(IPageLayout.ID_TASK_LIST);
+		bottom.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
 				
+		// TODO: for when we have outline view content
+//		IFolderLayout outlineFolder = 
+//			factory.createFolder(
+//					"right", //$NON-NLS-1$
+//					IPageLayout.RIGHT,
+//					(float)0.75,
+//					factory.getEditorArea());
+//		outlineFolder.addView(IPageLayout.ID_OUTLINE);
+
 		factory.addFastView("org.eclipse.team.sync.views.SynchronizeView", 0.50f); //NON-NLS-1
 	}
 
@@ -74,30 +93,38 @@ public class GoPerspective implements IPerspectiveFactory {
 		factory.addActionSet("org.eclipse.debug.ui.profileActionSet"); //NON-NLS-1
 		factory.addActionSet("org.eclipse.team.ui.actionSet"); //NON-NLS-1
 		factory.addActionSet("org.eclipse.ant.ui.actionSet.presentation"); //NON-NLS-1
+		factory.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+		
+		// TODO: should probably remove these
 		factory.addActionSet(JavaUI.ID_ACTION_SET);
 		factory.addActionSet(JavaUI.ID_ELEMENT_CREATION_ACTION_SET);
-		factory.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET); //NON-NLS-1
 	}
 
 	private void addPerspectiveShortcuts() {
-//		factory.addPerspectiveShortcut("org.eclipse.team.ui.TeamSynchronizingPerspective"); //NON-NLS-1
-//		factory.addPerspectiveShortcut("org.eclipse.team.cvs.ui.cvsPerspective"); //NON-NLS-1
-//		factory.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective"); //NON-NLS-1
+		factory.addPerspectiveShortcut("org.eclipse.team.ui.TeamSynchronizingPerspective"); //NON-NLS-1
+		factory.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective"); //NON-NLS-1
+		factory.addPerspectiveShortcut(IDebugUIConstants.ID_DEBUG_PERSPECTIVE);
 	}
 
 	private void addNewWizardShortcuts() {
-		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//NON-NLS-1
-		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//NON-NLS-1
+		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder"); //NON-NLS-1
+		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.file"); //NON-NLS-1
+		factory.addNewWizardShortcut("org.eclipse.ui.editors.wizards.UntitledTextFileWizard"); //$NON-NLS-1$
+		factory.addNewWizardShortcut("com.googlecode.goclipse.wizards.project.mainwiz"); //NON-NLS-1
+		factory.addNewWizardShortcut("com.googlecode.goclipse.wizards.NewGoFileWizard"); //NON-NLS-1
 	}
 
+	@SuppressWarnings("deprecation")
 	private void addViewShortcuts() {
-		factory.addShowViewShortcut("org.eclipse.pde.ui.DependenciesView"); //NON-NLS-1
 		factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); //NON-NLS-1
 		factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-		factory.addShowViewShortcut(JavaUI.ID_PACKAGES);
+		factory.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
 		factory.addShowViewShortcut(IPageLayout.ID_RES_NAV);
 		factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
 		factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+		factory.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+		factory.addShowViewShortcut("org.eclipse.search.ui.views.SearchView");
+		factory.addShowViewShortcut("org.eclipse.pde.runtime.LogView"); //$NON-NLS-1$
 	}
 
 }
