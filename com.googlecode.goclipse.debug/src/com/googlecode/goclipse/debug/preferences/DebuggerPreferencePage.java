cc3215a5384a6706d9379b1e2b0ff178c90ec2fa
devoncarew@gmail.com <devoncarew@gmail.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2012-02-04 07:02:56 +0000

    cleaned up the UI of the debugger pref page added project save actions to remove unused imports and other minor cleanups
    git-svn-id: https://goclipse.googlecode.com/svn/trunk@275 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- com.googlecode.goclipse.debug/src/com/googlecode/goclipse/debug/preferences/DebuggerPreferencePage.java
+++ com.googlecode.goclipse.debug/src/com/googlecode/goclipse/debug/preferences/DebuggerPreferencePage.java
@@ -1,37 +1,50 @@
 package com.googlecode.goclipse.debug.preferences;
 
+import com.googlecode.goclipse.Activator;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-import com.googlecode.goclipse.Activator;
-
 /**
  * The preference page for the Go debugger.
- * 
+ *
  * @author devoncarew
  */
 public class DebuggerPreferencePage extends FieldEditorPreferencePage implements
-		IWorkbenchPreferencePage {
+    IWorkbenchPreferencePage {
 
-	public DebuggerPreferencePage() {
-		super(GRID);
+  public DebuggerPreferencePage() {
+    super(GRID);
 
-		setPreferenceStore(Activator.getDefault().getPreferenceStore());
-	}
+    setPreferenceStore(Activator.getDefault().getPreferenceStore());
+  }
 
-	@Override
-	public void init(IWorkbench workbench) {
+  @Override
+  public void init(IWorkbench workbench) {
 
-	}
+  }
 
-	@Override
-	protected void createFieldEditors() {
-		FileFieldEditor fieldEditor = new FileFieldEditor(
-			"com.googlecode.goclipse.gdb", "GDB path:", getFieldEditorParent());
+  @Override
+  protected void createFieldEditors() {
+    Group group = new Group(getFieldEditorParent(), SWT.NONE);
+    group.setText("GDB Path");
+    GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.TOP).applyTo(group);
+    GridLayoutFactory.fillDefaults().margins(10, 4).applyTo(group);
 
-		addField(fieldEditor);
-	}
+    Composite fieldParent = new Composite(group, SWT.NONE);
+    GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.TOP).applyTo(fieldParent);
+
+    FileFieldEditor fieldEditor = new FileFieldEditor("com.googlecode.goclipse.gdb", "GDB path:",
+        fieldParent);
+
+    addField(fieldEditor);
+  }
 
 }
