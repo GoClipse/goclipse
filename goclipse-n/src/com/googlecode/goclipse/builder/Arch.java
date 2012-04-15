b06b06cdf0da8ea94d72fada4851ced728bfbbde
steel@kryas.com <steel@kryas.com@1731708e-d1f7-11de-a1df-87ee4daaac7b>
2012-04-15 07:59:41 +0000

    git-svn-id: https://goclipse.googlecode.com/svn/trunk@391 1731708e-d1f7-11de-a1df-87ee4daaac7b

--- goclipse-n/src/com/googlecode/goclipse/builder/Arch.java
+++ goclipse-n/src/com/googlecode/goclipse/builder/Arch.java
@@ -5,51 +5,59 @@ package com.googlecode.goclipse.builder;
  * @author steel
  */
 public enum Arch {
-   
-   x86(".8"),
-   amd64(".6"),
-   arm(".5");
-   
-   private String extension;
-   
-   /**
-    * @return the extension
-    */
-   public String getExtension() {
-      return extension;
-   }
 
-   /**
-    * @param extension the extension to set
-    */
-   public void setExtension(String extension) {
-      this.extension = extension;
-   }
+	x86  (".8", "8"),
+	amd64(".6", "6"),
+	arm  (".5", "5");
 
-   /**
-    * 
-    * @param extension
-    */
-   private Arch(String extension){
-      this.extension = extension;
-   }
-   
+	private String	extension;
+	private String	prefix;
 
-/**
-    * @param arch
-    * @return
-    */
-   public static Arch getArch(String arch){
-      if(amd64.name().equals(arch)){
-         return amd64;
-      }
-      else if(x86.name().equals(arch) || "386".equals(arch)){
-         return x86;
-      } 
-      else if(arm.name().equals(arch)){
-         return arm;
-      }
-      
-      return null;
-   }
+	/**
+	 * @return the extension
+	 */
+	public String getExtension() {
+		return extension;
+	}
+
+	/**
+	 * @param extension
+	 *            the extension to set
+	 */
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	/**
+	 * 
+	 * @param extension
+	 */
+	private Arch(String extension, String prefix) {
+		this.extension = extension;
+		this.prefix = prefix;
+	}
+
+	/**
+	 * @param arch
+	 * @return
+	 */
+	public static Arch getArch(String arch) {
+		if (amd64.name().equals(arch)) {
+			return amd64;
+		} else if (x86.name().equals(arch) || "386".equals(arch)) {
+			return x86;
+		} else if (arm.name().equals(arch)) {
+			return arm;
+		}
+
+		return null;
+	}
 }
