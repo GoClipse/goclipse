a37c9d969eb6212af5b208c083a5bc0607573096
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-01-30 12:44:24 +0000

    Added ui plugin

---
+++ com.googlecode.goclipse.ui/src/com/googlecode/goclipse/ui/GoUIPlugin.java
@@ -0,0 +1,12 @@
+package com.googlecode.goclipse.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class GoUIPlugin extends AbstractUIPlugin {
+	
+	public static final String PLUGIN_ID = "com.googlecode.goclipse.ui";
+	
+	public GoUIPlugin() {
+	}
+	
+}
