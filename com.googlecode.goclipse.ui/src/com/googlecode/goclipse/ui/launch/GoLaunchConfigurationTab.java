2944c4d56d17feab5c7e1713c67299db5f8d1b71
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-02-03 16:44:49 +0000

    Indentation normalized.

--- com.googlecode.goclipse.ui/src/com/googlecode/goclipse/ui/launch/GoLaunchConfigurationTab.java
+++ com.googlecode.goclipse.ui/src/com/googlecode/goclipse/ui/launch/GoLaunchConfigurationTab.java
@@ -26,175 +26,175 @@ import com.googlecode.goclipse.ui.GoUIPlugin;
  * @author steel
  */
 public class GoLaunchConfigurationTab extends AbstractLaunchConfigurationTab {
-  private GoLaunchConfigurationTabComposite composite;
-  private WorkingDirectoryBlock workingDirectoryBlock;
-
-  private String errorMessage = null;
-
-  public GoLaunchConfigurationTab() {
-	    workingDirectoryBlock = new LangWorkingDirectoryBlock();
-  }
-
-  @Override
-  public boolean canSave() {
-    return validate();
-  }
-
-  /**
-   * @return
-   */
-  protected boolean validate() {
-    // run the gauntlet
-    try {
-      if (composite == null) {
-        return false;
-      }
-
-      String project = composite.getProject();
-      if (project == null) {
-        errorMessage = "The user must set a valid Go project.";
-        composite.setMainConfigEnabled(false);
-        return false;
-      }
-
-      IProject iProject = ResourcesPlugin.getWorkspace().getRoot().getProject(project);
-
-      if (iProject == null) {
-        errorMessage = "The project folder given could not be found.";
-        composite.setMainConfigEnabled(false);
-        return false;
-      }
-
-      if (iProject.getNature(GoNature.NATURE_ID) == null) {
-        errorMessage = "The project does not have a Go Nature assigned to it.  "
-            + "Please reference valid Go project.";
-        composite.setMainConfigEnabled(false);
-        return false;
-      }
-
-      composite.setMainConfigEnabled(true);
-      IResource mainfile = null;
-
-      try {
-        mainfile = iProject.findMember(composite.getMainFile());
-      } catch (Exception e) {
-        GoUIPlugin.log(e);
-      }
-
-      if (mainfile == null) {
-        errorMessage = "The main file could not be found.  "
-            + "Please make sure the given path is correct and is relative to the project.";
-        return false;
-      }
-
-    } catch (CoreException e) {
-      GoUIPlugin.log(e);
-      
-      errorMessage = "A problem has occurred while attempting to validate the fields in this configuration.";
-    }
-    
-    errorMessage = null;
-    
-    return true;
-  }
-
-  @Override
-  public void createControl(Composite parent) {
-    composite = new GoLaunchConfigurationTabComposite(parent, this, SWT.NULL);
-    setControl(composite);
-    composite.setLaunchConfigurationDialog(getLaunchConfigurationDialog());
-
-    workingDirectoryBlock.createControl(composite);
-  }
-
-  @Override
-  public String getErrorMessage() {
-    if (errorMessage != null) {
-      return errorMessage;
-    } else {
-      return workingDirectoryBlock.getErrorMessage();
-    }
-  }
-  
-  @Override
-  public Image getImage() {
-    return GoPluginImages.getImage(GoPluginImages.GO_ICON);
-  }
-
-  @Override
-  public String getMessage() {
-    return "Configure a Go application to run.";
-  }
-
-  @Override
-  protected Button createPushButton(Composite parent, String label, Image image) {
-    return super.createPushButton(parent, label, image);
-  }
-
-  @Override
-  public String getName() {
-    return "Main";
-  }
-
-  @Override
-  public void initializeFrom(ILaunchConfiguration configuration) {
-    try {
-      String projectname = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "");
-      String mainfile = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
-      String buildconfig = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
-          "");
-      String programargs = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
-
-      composite.setProject(projectname);
-      composite.setMainFile(mainfile);
-      composite.setBuildConfig(buildconfig != null && !buildconfig.equals("")
-          ? BuildConfiguration.get(buildconfig) : BuildConfiguration.DEBUG);
-      composite.setProgramArgs(programargs);
-
-      workingDirectoryBlock.initializeFrom(configuration);
-    } catch (CoreException e) {
-      GoUIPlugin.log(e);
-    }
-  }
-
-  @Override
-  public boolean isValid(ILaunchConfiguration launchConfig) {
-    return validate() && workingDirectoryBlock.isValid(launchConfig);
-  }
-
-  @Override
-  public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, composite.getProject());
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, composite.getMainFile());
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
-        composite.getBuildConfiguration().toString());
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, composite.getProgramArgs());
-    configuration.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
-    configuration.setAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING, "UTF-8");
-    workingDirectoryBlock.performApply(configuration);
-  }
-
-  @Override
-  public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "enter a project here...");
-
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
-        BuildConfiguration.RELEASE.toString());
-    configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
-
-    workingDirectoryBlock.setDefaults(configuration);
-  }
-
-  @Override
-  public void setLaunchConfigurationDialog(ILaunchConfigurationDialog dialog) {
-    super.setLaunchConfigurationDialog(dialog);
-
-    if (composite != null) {
-      composite.setLaunchConfigurationDialog(dialog);
-    }
-
-    workingDirectoryBlock.setLaunchConfigurationDialog(dialog);
-  }
-
+	private GoLaunchConfigurationTabComposite composite;
+	private WorkingDirectoryBlock workingDirectoryBlock;
+	
+	private String errorMessage = null;
+	
+	public GoLaunchConfigurationTab() {
+		workingDirectoryBlock = new LangWorkingDirectoryBlock();
+	}
+	
+	@Override
+	public boolean canSave() {
+		return validate();
+	}
+	
+	/**
+	 * @return
+	 */
+	protected boolean validate() {
+		// run the gauntlet
+		try {
+			if (composite == null) {
+				return false;
+			}
+			
+			String project = composite.getProject();
+			if (project == null) {
+				errorMessage = "The user must set a valid Go project.";
+				composite.setMainConfigEnabled(false);
+				return false;
+			}
+			
+			IProject iProject = ResourcesPlugin.getWorkspace().getRoot().getProject(project);
+			
+			if (iProject == null) {
+				errorMessage = "The project folder given could not be found.";
+				composite.setMainConfigEnabled(false);
+				return false;
+			}
+			
+			if (iProject.getNature(GoNature.NATURE_ID) == null) {
+				errorMessage = "The project does not have a Go Nature assigned to it.  "
+						+ "Please reference valid Go project.";
+				composite.setMainConfigEnabled(false);
+				return false;
+			}
+			
+			composite.setMainConfigEnabled(true);
+			IResource mainfile = null;
+			
+			try {
+				mainfile = iProject.findMember(composite.getMainFile());
+			} catch (Exception e) {
+				GoUIPlugin.log(e);
+			}
+			
+			if (mainfile == null) {
+				errorMessage = "The main file could not be found.  "
+						+ "Please make sure the given path is correct and is relative to the project.";
+				return false;
+			}
+			
+		} catch (CoreException e) {
+			GoUIPlugin.log(e);
+			
+			errorMessage = "A problem has occurred while attempting to validate the fields in this configuration.";
+		}
+		
+		errorMessage = null;
+		
+		return true;
+	}
+	
+	@Override
+	public void createControl(Composite parent) {
+		composite = new GoLaunchConfigurationTabComposite(parent, this, SWT.NULL);
+		setControl(composite);
+		composite.setLaunchConfigurationDialog(getLaunchConfigurationDialog());
+		
+		workingDirectoryBlock.createControl(composite);
+	}
+	
+	@Override
+	public String getErrorMessage() {
+		if (errorMessage != null) {
+			return errorMessage;
+		} else {
+			return workingDirectoryBlock.getErrorMessage();
+		}
+	}
+	
+	@Override
+	public Image getImage() {
+		return GoPluginImages.getImage(GoPluginImages.GO_ICON);
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Configure a Go application to run.";
+	}
+	
+	@Override
+	protected Button createPushButton(Composite parent, String label, Image image) {
+		return super.createPushButton(parent, label, image);
+	}
+	
+	@Override
+	public String getName() {
+		return "Main";
+	}
+	
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		try {
+			String projectname = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "");
+			String mainfile = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
+			String buildconfig = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
+					"");
+			String programargs = configuration.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
+			
+			composite.setProject(projectname);
+			composite.setMainFile(mainfile);
+			composite.setBuildConfig(buildconfig != null && !buildconfig.equals("")
+					? BuildConfiguration.get(buildconfig) : BuildConfiguration.DEBUG);
+			composite.setProgramArgs(programargs);
+			
+			workingDirectoryBlock.initializeFrom(configuration);
+		} catch (CoreException e) {
+			GoUIPlugin.log(e);
+		}
+	}
+	
+	@Override
+	public boolean isValid(ILaunchConfiguration launchConfig) {
+		return validate() && workingDirectoryBlock.isValid(launchConfig);
+	}
+	
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, composite.getProject());
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, composite.getMainFile());
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
+				composite.getBuildConfiguration().toString());
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, composite.getProgramArgs());
+		configuration.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
+		configuration.setAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING, "UTF-8");
+		workingDirectoryBlock.performApply(configuration);
+	}
+	
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "enter a project here...");
+		
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
+				BuildConfiguration.RELEASE.toString());
+		configuration.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
+		
+		workingDirectoryBlock.setDefaults(configuration);
+	}
+	
+	@Override
+	public void setLaunchConfigurationDialog(ILaunchConfigurationDialog dialog) {
+		super.setLaunchConfigurationDialog(dialog);
+		
+		if (composite != null) {
+			composite.setLaunchConfigurationDialog(dialog);
+		}
+		
+		workingDirectoryBlock.setLaunchConfigurationDialog(dialog);
+	}
+	
 }
--- com.googlecode.goclipse.ui/src/com/googlecode/goclipse/ui/launch/GoLaunchShortcut.java
+++ com.googlecode.goclipse.ui/src/com/googlecode/goclipse/ui/launch/GoLaunchShortcut.java
@@ -24,86 +24,86 @@ import com.googlecode.goclipse.builder.GoConstants;
  * @author steel
  */
 public class GoLaunchShortcut implements ILaunchShortcut {
-
-  @Override
-  public void launch(ISelection selection, final String mode) {
-    Activator.logInfo("launch shortcut: selection " + selection + " mode " + mode);
-    
-    if (selection != null) {
-      if (selection instanceof TreeSelection) {
-        TreeSelection ts = (TreeSelection) selection;
-        
-        Object e = ts.getFirstElement();
-        
-        if (e instanceof IFile) {
-          IFile file = (IFile) e;
-          final String fName = file.getProjectRelativePath().toOSString();
-          final String pName = file.getProject().getName();
-          launch(pName, fName, mode);
-        }
-      }
-    }
-  }
-
-  @Override
-  public void launch(IEditorPart editor, final String mode) {
-    Activator.logInfo("launch shortcut: editor" + editor.getTitle() + " mode " + mode);
-    
-    IEditorInput ei = editor.getEditorInput();
-    
-    if (ei != null && ei instanceof FileEditorInput) {
-      FileEditorInput fei = (FileEditorInput) ei;
-      IFile file = fei.getFile();
-      final String fName = file.getProjectRelativePath().toOSString();
-      final String pName = file.getProject().getName();
-      launch(pName, fName, mode);
-    }
-  }
-
-  private void launch(String pName, String fName, String mode) {
-    try {
-      ILaunchConfiguration found = null;
-      ILaunchManager lm = DebugPlugin.getDefault().getLaunchManager();
-      ILaunchConfigurationType lct = lm.getLaunchConfigurationType(LaunchConstants.LAUNCH_CONFIG_ID);
-      ILaunchConfiguration[] lcfgs = lm.getLaunchConfigurations(lct);
-      
-      for (ILaunchConfiguration lcf : lcfgs) {
-        String project = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "");
-        String mainfile = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
-        String prgArgs = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
-        
-        if (prgArgs.isEmpty()) {
-          // this is an empty run, no params, don't mix with already
-          // definded with params
-          if (project.equalsIgnoreCase(pName)
-              && Path.fromOSString(fName).equals(Path.fromOSString(mainfile))) {
-            found = lcf;
-            break;
-          }
-        }
-      }
-      
-      if (found == null) {
-        //create a new launch configuration
-        String cfgName = lm.generateLaunchConfigurationName(Path.fromOSString(fName).lastSegment());
-        ILaunchConfigurationWorkingCopy workingCopy = lct.newInstance(null, cfgName);
-        workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, pName);
-        workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, fName);
-        workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
-        workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
-            BuildConfiguration.RELEASE.toString());
-        workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
-        workingCopy.setAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING, "UTF-8");
-
-        found = workingCopy.doSave();
-      }
-      
-      if (found != null) {
-        found.launch(mode, null, true, true);
-      }
-    } catch (CoreException ce) {
-      Activator.logError(ce);
-    }
-  }
-
+	
+	@Override
+	public void launch(ISelection selection, final String mode) {
+		Activator.logInfo("launch shortcut: selection " + selection + " mode " + mode);
+		
+		if (selection != null) {
+			if (selection instanceof TreeSelection) {
+				TreeSelection ts = (TreeSelection) selection;
+				
+				Object e = ts.getFirstElement();
+				
+				if (e instanceof IFile) {
+					IFile file = (IFile) e;
+					final String fName = file.getProjectRelativePath().toOSString();
+					final String pName = file.getProject().getName();
+					launch(pName, fName, mode);
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void launch(IEditorPart editor, final String mode) {
+		Activator.logInfo("launch shortcut: editor" + editor.getTitle() + " mode " + mode);
+		
+		IEditorInput ei = editor.getEditorInput();
+		
+		if (ei != null && ei instanceof FileEditorInput) {
+			FileEditorInput fei = (FileEditorInput) ei;
+			IFile file = fei.getFile();
+			final String fName = file.getProjectRelativePath().toOSString();
+			final String pName = file.getProject().getName();
+			launch(pName, fName, mode);
+		}
+	}
+	
+	private void launch(String pName, String fName, String mode) {
+		try {
+			ILaunchConfiguration found = null;
+			ILaunchManager lm = DebugPlugin.getDefault().getLaunchManager();
+			ILaunchConfigurationType lct = lm.getLaunchConfigurationType(LaunchConstants.LAUNCH_CONFIG_ID);
+			ILaunchConfiguration[] lcfgs = lm.getLaunchConfigurations(lct);
+			
+			for (ILaunchConfiguration lcf : lcfgs) {
+				String project = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, "");
+				String mainfile = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, "");
+				String prgArgs = lcf.getAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
+				
+				if (prgArgs.isEmpty()) {
+					// this is an empty run, no params, don't mix with already
+					// definded with params
+					if (project.equalsIgnoreCase(pName)
+							&& Path.fromOSString(fName).equals(Path.fromOSString(mainfile))) {
+						found = lcf;
+						break;
+					}
+				}
+			}
+			
+			if (found == null) {
+				//create a new launch configuration
+				String cfgName = lm.generateLaunchConfigurationName(Path.fromOSString(fName).lastSegment());
+				ILaunchConfigurationWorkingCopy workingCopy = lct.newInstance(null, cfgName);
+				workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_PROJECT, pName);
+				workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_MAIN, fName);
+				workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_ARGS, "");
+				workingCopy.setAttribute(GoConstants.GO_CONF_ATTRIBUTE_BUILD_CONFIG,
+						BuildConfiguration.RELEASE.toString());
+				workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
+				workingCopy.setAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING, "UTF-8");
+				
+				found = workingCopy.doSave();
+			}
+			
+			if (found != null) {
+				found.launch(mode, null, true, true);
+			}
+		} catch (CoreException ce) {
+			Activator.logError(ce);
+		}
+	}
+	
 }
