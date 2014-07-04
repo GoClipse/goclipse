6f52e4b92e3c931e8506314c90a4434baf97da8b
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-07-04 20:01:04 +0100

    udpate OS constants.

--- plugin_tooling/src-util/melnorme/utilbox/misc/MiscUtil.java
+++ plugin_tooling/src-util/melnorme/utilbox/misc/MiscUtil.java
@@ -28,9 +28,11 @@ import melnorme.utilbox.core.fntypes.Predicate;
 
 public class MiscUtil {
 	
-	public static final String OS_NAME = System.getProperty("os.name");
+	public static final String OS_NAME = StringUtil.nullAsEmpty(System.getProperty("os.name"));
 	
 	public static final boolean OS_IS_WINDOWS = OS_NAME.startsWith("Windows");
+	public static final boolean OS_IS_LINUX = OS_NAME.startsWith("Linux") || OS_NAME.startsWith("LINUX");
+	public static final boolean OS_IS_MAC = OS_NAME.startsWith("Mac");
 	
 	public static <T> Predicate<T> getNotNullPredicate() {
 		return new NotNullPredicate<T>();
