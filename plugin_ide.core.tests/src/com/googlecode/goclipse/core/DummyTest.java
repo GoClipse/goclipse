cb22d521336f0d2f47f72a219318b302b2d7c2c0
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-07-04 18:48:20 +0100

    More project structure refactoring to be like BareboneLangIDE: added tooling and core tests plugins.

---
+++ plugin_ide.core.tests/src/com/googlecode/goclipse/core/DummyTest.java
@@ -0,0 +1,13 @@
+package com.googlecode.goclipse.core;
+
+import org.junit.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void testname() throws Exception { testname$(); }
+	public void testname$() throws Exception {
+		 
+	}
+	
+}
