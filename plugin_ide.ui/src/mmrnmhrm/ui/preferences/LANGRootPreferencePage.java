7d7cc70b5515b82bd9465d4bf19e7fcf5a511e73
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-09-08 17:52:09 +0100

    Add some preference pages

---
+++ plugin_ide.ui/src/mmrnmhrm/ui/preferences/LANGRootPreferencePage.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2014 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
+package mmrnmhrm.ui.preferences;
+
+import melnorme.lang.ide.ui.LangUIPlugin;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+
+/**
+ * The root preference page for DDT 
+ */
+public class LANGRootPreferencePage extends PreferencePage implements IWorkbenchPreferencePage{
+	
+	public LANGRootPreferencePage() {
+		setPreferenceStore(LangUIPlugin.getPrefStore());
+	}
+	
+	@Override
+	public void init(IWorkbench workbench) {
+		// Nothing to do
+	}
+	
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite content = new Composite(parent, SWT.NONE);
+		return content;
+	}
+	
+}
