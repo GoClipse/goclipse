ad310b1383e622c321c71b4459525c1078121307
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-09-10 20:58:00 +0100

    GoScanner cleanup: removed CombinedWord usage.

--- plugin_ide.ui/src/com/googlecode/goclipse/ui/text/GoScanner.java
+++ plugin_ide.ui/src/com/googlecode/goclipse/ui/text/GoScanner.java
@@ -9,9 +9,9 @@ import org.eclipse.cdt.ui.text.ITokenStoreFactory;
 import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.WhitespaceRule;
+import org.eclipse.jface.text.rules.WordRule;
 
 import com.googlecode.goclipse.ui.GoUIPreferenceConstants;
-import com.googlecode.goclipse.ui.text.CombinedWordRule.WordMatcher;
 
 public class GoScanner extends AbstractLangScanner {
 	
@@ -36,166 +36,137 @@ public class GoScanner extends AbstractLangScanner {
 		
 		final IToken tkDefault = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__TEXT.key);
 		
-		final WordMatcher keywordRule = new WordMatcher();
+		WordRule wordRule = new WordRule(new JavaWordDetector(), tkDefault);
 		
-		// TODO deprecate CombinedWordRule usage, use only J
-		CombinedWordRule combinedWordRule = new CombinedWordRule(new JavaWordDetector(), keywordRule, tkDefault);
-		
-
 		final IToken keyword         = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__KEYWORD.key);
 		final IToken value           = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__VALUE.key);
 		final IToken primitive       = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__PRIMITIVE.key);
 		final IToken builtinFunction = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__BUILTIN_FUNCTION.key);
-		final IToken operator        = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__OPERATOR.key);
 		final IToken textToken       = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__TEXT.key);
-		final IToken stringDelimeter = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__STRING.key); 
 		setDefaultReturnToken(textToken);
 		
 		// add tokens for each reserved word
-		keywordRule.addWord("break",       keyword);
-		keywordRule.addWord("default",     keyword);
-		keywordRule.addWord("func",        keyword);
-		keywordRule.addWord("interface",   keyword);
-		keywordRule.addWord("select",      keyword);
-		keywordRule.addWord("case",        keyword);
-		keywordRule.addWord("defer",       keyword);
-		keywordRule.addWord("go",          keyword);
-		keywordRule.addWord("map",         keyword);
-		keywordRule.addWord("struct",      keyword);
-		keywordRule.addWord("chan",        keyword);
-		keywordRule.addWord("else",        keyword);
-		keywordRule.addWord("goto",        keyword);
-		keywordRule.addWord("package",     keyword);
-		keywordRule.addWord("switch",      keyword);
-		keywordRule.addWord("const",       keyword);
-		keywordRule.addWord("fallthrough", keyword);
-		keywordRule.addWord("if",          keyword);
-		keywordRule.addWord("range",       keyword);
-		keywordRule.addWord("type",        keyword);
-		keywordRule.addWord("continue",    keyword);
-		keywordRule.addWord("for",         keyword);
-		keywordRule.addWord("import",      keyword);
-		keywordRule.addWord("return",      keyword);
-		keywordRule.addWord("var",         keyword);
+		wordRule.addWord("break",       keyword);
+		wordRule.addWord("default",     keyword);
+		wordRule.addWord("func",        keyword);
+		wordRule.addWord("interface",   keyword);
+		wordRule.addWord("select",      keyword);
+		wordRule.addWord("case",        keyword);
+		wordRule.addWord("defer",       keyword);
+		wordRule.addWord("go",          keyword);
+		wordRule.addWord("map",         keyword);
+		wordRule.addWord("struct",      keyword);
+		wordRule.addWord("chan",        keyword);
+		wordRule.addWord("else",        keyword);
+		wordRule.addWord("goto",        keyword);
+		wordRule.addWord("package",     keyword);
+		wordRule.addWord("switch",      keyword);
+		wordRule.addWord("const",       keyword);
+		wordRule.addWord("fallthrough", keyword);
+		wordRule.addWord("if",          keyword);
+		wordRule.addWord("range",       keyword);
+		wordRule.addWord("type",        keyword);
+		wordRule.addWord("continue",    keyword);
+		wordRule.addWord("for",         keyword);
+		wordRule.addWord("import",      keyword);
+		wordRule.addWord("return",      keyword);
+		wordRule.addWord("var",         keyword);
 
-		keywordRule.addWord("append",  builtinFunction);
-		keywordRule.addWord("cap",     builtinFunction);
-		keywordRule.addWord("close",   builtinFunction);
-		keywordRule.addWord("complex", builtinFunction);
-		keywordRule.addWord("copy",    builtinFunction);
-		keywordRule.addWord("delete",  builtinFunction);
-		keywordRule.addWord("imag",    builtinFunction);
-		keywordRule.addWord("len",     builtinFunction);
-		keywordRule.addWord("make",    builtinFunction);
-		keywordRule.addWord("new",     builtinFunction);
-		keywordRule.addWord("panic",   builtinFunction);
-		keywordRule.addWord("print",   builtinFunction);
-		keywordRule.addWord("println", builtinFunction);
-		keywordRule.addWord("real",    builtinFunction);
-		keywordRule.addWord("recover", builtinFunction);
+		wordRule.addWord("append",  builtinFunction);
+		wordRule.addWord("cap",     builtinFunction);
+		wordRule.addWord("close",   builtinFunction);
+		wordRule.addWord("complex", builtinFunction);
+		wordRule.addWord("copy",    builtinFunction);
+		wordRule.addWord("delete",  builtinFunction);
+		wordRule.addWord("imag",    builtinFunction);
+		wordRule.addWord("len",     builtinFunction);
+		wordRule.addWord("make",    builtinFunction);
+		wordRule.addWord("new",     builtinFunction);
+		wordRule.addWord("panic",   builtinFunction);
+		wordRule.addWord("print",   builtinFunction);
+		wordRule.addWord("println", builtinFunction);
+		wordRule.addWord("real",    builtinFunction);
+		wordRule.addWord("recover", builtinFunction);
 
-		keywordRule.addWord("nil",   value);
-		keywordRule.addWord("true",  value);
-		keywordRule.addWord("false", value);
-		keywordRule.addWord("iota",  value);
+		wordRule.addWord("nil",   value);
+		wordRule.addWord("true",  value);
+		wordRule.addWord("false", value);
+		wordRule.addWord("iota",  value);
 
-		keywordRule.addWord("uint8", primitive); // the set of all unsigned
-												 // 8-bit integers (0 to
-												 // 255)
-		keywordRule.addWord("uint16", primitive); // the set of all unsigned
-												  // 16-bit integers (0 to
-												  // 65535)
-		keywordRule.addWord("uint32", primitive); // the set of all unsigned
-												  // 32-bit integers (0 to
-												  // 4294967295)
-		keywordRule.addWord("uint64", primitive); // the set of all unsigned
-												  // 64-bit integers (0 to
-												  // 18446744073709551615)
-		keywordRule.addWord("int8", primitive); // the set of all signed
-												// 8-bit integers (-128 to
-												// 127)
-		keywordRule.addWord("int16", primitive); // the set of all signed
-												 // 16-bit integers (-32768
-												 // to 32767)
-		keywordRule.addWord("int32", primitive); // the set of all signed
-												 // 32-bit integers
-												 // (-2147483648 to
-												 // 2147483647)
-		keywordRule.addWord("int64", primitive); // the set of all signed
-												 // 64-bit integers
-												 // (-9223372036854775808 to
-												 // 9223372036854775807)
-		keywordRule.addWord("float32", primitive); // the set of all
-												   // IEEE-754 32-bit
-												   // floating-point numbers
-		keywordRule.addWord("float64", primitive); // the set of all
-												   // IEEE-754 64-bit
-												   // floating-point numbers
-		keywordRule.addWord("complex64",  primitive);
-		keywordRule.addWord("complex128", primitive);
-		keywordRule.addWord("rune",       primitive);
-		keywordRule.addWord("byte",       primitive); // familiar alias for uint8
-		keywordRule.addWord("uint",       primitive); // either 32 or 64 bits
-		keywordRule.addWord("int",        primitive); // either 32 or 64 bits
-		keywordRule.addWord("uintptr",    primitive); // an unsigned integer
-												   // large enough to store
-												   // the uninterpreted bits
-												   // of a pointer value
-		keywordRule.addWord("string", primitive);
-		keywordRule.addWord("bool",   primitive);
-		keywordRule.addWord("error",  primitive);
-
-		keywordRule.addWord("`",    stringDelimeter);
-		keywordRule.addWord("\"",   stringDelimeter);
+		wordRule.addWord("uint8", primitive);
+		wordRule.addWord("uint16", primitive);
+		wordRule.addWord("uint32", primitive);
+		wordRule.addWord("uint64", primitive);
+		wordRule.addWord("int8", primitive);
+		wordRule.addWord("int16", primitive);
+		wordRule.addWord("int32", primitive);
+		wordRule.addWord("int64", primitive);
+		wordRule.addWord("float32", primitive);
+		wordRule.addWord("float64", primitive);
+		wordRule.addWord("complex64",  primitive);
+		wordRule.addWord("complex128", primitive);
+		wordRule.addWord("rune",       primitive);
+		wordRule.addWord("byte",       primitive);
+		wordRule.addWord("uint",       primitive);
+		wordRule.addWord("int",        primitive);
+		wordRule.addWord("uintptr",    primitive);
 		
-		keywordRule.addWord("+",   operator);
-		keywordRule.addWord("&",   operator);
-		keywordRule.addWord("+=",  operator);
-		keywordRule.addWord("&=",  operator);
-		keywordRule.addWord("&&",  operator);
-		keywordRule.addWord("==",  operator);
-		keywordRule.addWord("!=",  operator);
-		keywordRule.addWord("(",   operator);
-		keywordRule.addWord(")",   operator);
-		keywordRule.addWord("-",   operator);
-		keywordRule.addWord("|",   operator);
-		keywordRule.addWord("-=",  operator);
-		keywordRule.addWord("|=",  operator);
-		keywordRule.addWord("||",  operator);
-		keywordRule.addWord("<",   operator);
-		keywordRule.addWord("<=",  operator);
-		keywordRule.addWord("[",   operator);
-		keywordRule.addWord("]",   operator);
-		keywordRule.addWord("*",   operator);
-		keywordRule.addWord("^",   operator);
-		keywordRule.addWord("*=",  operator);
-		keywordRule.addWord("^=",  operator);
-		keywordRule.addWord("<-",  operator);
-		keywordRule.addWord(">",   operator);
-		keywordRule.addWord(">=",  operator);
-		keywordRule.addWord("{",   operator);
-		keywordRule.addWord("}",   operator);
-		keywordRule.addWord("/",   operator);
-		keywordRule.addWord("<<",  operator);
-		keywordRule.addWord("/=",  operator);
-		keywordRule.addWord("<<=", operator);
-		keywordRule.addWord("++",  operator);
-		keywordRule.addWord("=",   operator);
-		keywordRule.addWord(":=",  operator);
-		keywordRule.addWord(",",   operator);
-		keywordRule.addWord(";",   operator);
-		keywordRule.addWord("%",   operator);
-		keywordRule.addWord(">>",  operator);
-		keywordRule.addWord("%=",  operator);
-		keywordRule.addWord(">>=", operator);
-		keywordRule.addWord("--",  operator);
-		keywordRule.addWord("!",   operator);
-		keywordRule.addWord("...", operator);
-		keywordRule.addWord(":",   operator);
-		keywordRule.addWord("&^",  operator);
-		keywordRule.addWord("&^=", operator);
+		wordRule.addWord("string", primitive);
+		wordRule.addWord("bool",   primitive);
+		wordRule.addWord("error",  primitive);
 		
-		rules.add(combinedWordRule);
+		// Note: operator matching is not actually working
+		
+		final IToken operator        = getToken(GoUIPreferenceConstants.SYNTAX_COLORING__OPERATOR.key);
+
+		wordRule.addWord("+",   operator);
+		wordRule.addWord("&",   operator);
+		wordRule.addWord("+=",  operator);
+		wordRule.addWord("&=",  operator);
+		wordRule.addWord("&&",  operator);
+		wordRule.addWord("==",  operator);
+		wordRule.addWord("!=",  operator);
+		wordRule.addWord("(",   operator);
+		wordRule.addWord(")",   operator);
+		wordRule.addWord("-",   operator);
+		wordRule.addWord("|",   operator);
+		wordRule.addWord("-=",  operator);
+		wordRule.addWord("|=",  operator);
+		wordRule.addWord("||",  operator);
+		wordRule.addWord("<",   operator);
+		wordRule.addWord("<=",  operator);
+		wordRule.addWord("[",   operator);
+		wordRule.addWord("]",   operator);
+		wordRule.addWord("*",   operator);
+		wordRule.addWord("^",   operator);
+		wordRule.addWord("*=",  operator);
+		wordRule.addWord("^=",  operator);
+		wordRule.addWord("<-",  operator);
+		wordRule.addWord(">",   operator);
+		wordRule.addWord(">=",  operator);
+		wordRule.addWord("{",   operator);
+		wordRule.addWord("}",   operator);
+		wordRule.addWord("/",   operator);
+		wordRule.addWord("<<",  operator);
+		wordRule.addWord("/=",  operator);
+		wordRule.addWord("<<=", operator);
+		wordRule.addWord("++",  operator);
+		wordRule.addWord("=",   operator);
+		wordRule.addWord(":=",  operator);
+		wordRule.addWord(",",   operator);
+		wordRule.addWord(";",   operator);
+		wordRule.addWord("%",   operator);
+		wordRule.addWord(">>",  operator);
+		wordRule.addWord("%=",  operator);
+		wordRule.addWord(">>=", operator);
+		wordRule.addWord("--",  operator);
+		wordRule.addWord("!",   operator);
+		wordRule.addWord("...", operator);
+		wordRule.addWord(":",   operator);
+		wordRule.addWord("&^",  operator);
+		wordRule.addWord("&^=", operator);
+		
+		rules.add(wordRule);
 		
 		setRules(rules);
 	}
