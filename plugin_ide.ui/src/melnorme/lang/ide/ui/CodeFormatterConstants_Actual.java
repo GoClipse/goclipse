4001e6ba8e09da7f9bca17278d544730fe78dd71
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-09-30 14:58:25 +0100

    Fix defaults init NPE.

--- plugin_ide.ui/src/melnorme/lang/ide/ui/CodeFormatterConstants_Actual.java
+++ plugin_ide.ui/src/melnorme/lang/ide/ui/CodeFormatterConstants_Actual.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2014 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
 package melnorme.lang.ide.ui;
 
 import melnorme.lang.ide.core.utils.prefs.IntPreference;
 import melnorme.lang.ide.core.utils.prefs.StringPreference;
+import melnorme.utilbox.misc.MiscUtil;
 
 
 public interface CodeFormatterConstants_Actual {
@@ -23,10 +34,9 @@ public interface CodeFormatterConstants_Actual {
 	class Helper {
 		
 		public static void initDefaults() {
-			// This will ensure default value is initialized
-			FORMATTER_INDENT_MODE.get();
-			FORMATTER_TAB_SIZE.get();
-			FORMATTER_INDENTATION_SPACES_SIZE.get();
+			// Ensure all default values are initialized, in case prefs are accessed by means other
+			// than by referencing the constants above 
+			MiscUtil.loadClass(LangAutoEditPreferenceConstants_Actual.class);
 		}
 		
 	}
