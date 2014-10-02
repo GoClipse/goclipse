a91308789652508960fd09a89b7872060af3694c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-10-02 13:24:56 +0100

    indentation

--- plugin_ide.ui/src/com/googlecode/goclipse/editors/GoEditor.java
+++ plugin_ide.ui/src/com/googlecode/goclipse/editors/GoEditor.java
@@ -25,148 +25,145 @@ import com.googlecode.goclipse.ui.GoUIPreferenceConstants;
 import com.googlecode.goclipse.ui.editor.GoEditorSourceViewerConfiguration;
 
 public class GoEditor extends AbstractLangEditor {
-
-  private static final String BUNDLE_ID = "com.googlecode.goclipse.editors.GoEditorMessages";
-  
-  private static ResourceBundle editorResourceBundle = ResourceBundle.getBundle(BUNDLE_ID);
-
-  private DefaultCharacterPairMatcher matcher;
-  private EditorImageUpdater imageUpdater;
-
-  private GoEditorOutlinePage outlinePage;
-
-  /**
-   * 
-   */
-  public GoEditor() {
-	  super();
-  }
-
+	
+	private static final String BUNDLE_ID = "com.googlecode.goclipse.editors.GoEditorMessages";
+	
+	private static ResourceBundle editorResourceBundle = ResourceBundle.getBundle(BUNDLE_ID);
+	
+	private DefaultCharacterPairMatcher matcher;
+	private EditorImageUpdater imageUpdater;
+	
+	private GoEditorOutlinePage outlinePage;
+	
+	public GoEditor() {
+		super();
+	}
+	
 	@Override
 	protected GoEditorSourceViewerConfiguration createSourceViewerConfiguration() {
 		return new GoEditorSourceViewerConfiguration(getPreferenceStore(), 
 			LangUIPlugin.getInstance().getColorManager(), this);
 	}
-
-  
-  @Override
-  protected void setTitleImage(Image image) {
-    super.setTitleImage(image);
-  }
-
-  @Override
-  public Object getAdapter(Class required) {
-    if (IContentOutlinePage.class.equals(required)) {
-      if (outlinePage == null) {
-        outlinePage = new GoEditorOutlinePage(getDocumentProvider(), this);
-      }
-
-      return outlinePage;
-    }
-
-    return super.getAdapter(required);
-  }
-
-  @Override
-  protected final void doSetInput(IEditorInput input) throws CoreException {
-    super.doSetInput(input);
-
-    if (input instanceof IFileEditorInput) {
-      imageUpdater = new EditorImageUpdater(this);
-    }
-  }
-
-  @Override
-  protected void configureSourceViewerDecorationSupport(SourceViewerDecorationSupport support) {
-    super.configureSourceViewerDecorationSupport(support);
-
-    char[] matchChars = {'(', ')', '[', ']', '{', '}'}; //which brackets to match
-    matcher = new DefaultCharacterPairMatcher(matchChars, IDocumentExtension3.DEFAULT_PARTITIONING);
-    support.setCharacterPairMatcher(matcher);
-    support.setMatchingCharacterPainterPreferenceKeys(
-    	GoUIPreferenceConstants.EDITOR_MATCHING_BRACKETS,
-    	GoUIPreferenceConstants.EDITOR_MATCHING_BRACKETS_COLOR);
-  }
-
-  @Override
-  protected void createActions() {
-    super.createActions();
-
-    IAction action;
-
-    action = new ToggleCommentAction(editorResourceBundle, "ToggleComment.", this);
-    action.setActionDefinitionId("com.googlecode.goclipse.actions.ToggleComment");
-    setAction("ToggleComment", action);
-    markAsStateDependentAction("ToggleComment", true);
-    configureToggleCommentAction();
-  }
-
-  public DefaultCharacterPairMatcher getPairMatcher() {
-    return matcher;
-  }
-
-  public String getText() {
-    return getSourceViewer().getDocument().get();
-  }
-
-  public void replaceText(String newText) {
-    ISelection sel = getSelectionProvider().getSelection();
-    int topIndex = getSourceViewer().getTopIndex();
-
-    getSourceViewer().getDocument().set(newText);
-
-    if (sel != null) {
-      getSelectionProvider().setSelection(sel);
-    }
-
-    if (topIndex != -1) {
-      getSourceViewer().setTopIndex(topIndex);
-    }
-  }
-
-  @Override
-  public void dispose() {
-
-    if (imageUpdater != null) {
-      imageUpdater.dispose();
-    }
-
-    super.dispose();
-  }
-
-  private void configureToggleCommentAction() {
-    IAction action = getAction("ToggleComment");
-
-    if (action instanceof ToggleCommentAction) {
-      ISourceViewer sourceViewer = getSourceViewer();
-      SourceViewerConfiguration configuration = getSourceViewerConfiguration();
-      ((ToggleCommentAction) action).configure(sourceViewer, configuration);
-    }
-  }
-
-  protected void handleReconcilation(IRegion partition) {
-    if (outlinePage != null) {
-      outlinePage.handleEditorReconcilation();
-    }
-  }
-
+	
+	
+	@Override
+	protected void setTitleImage(Image image) {
+		super.setTitleImage(image);
+	}
+	
+	@Override
+	public Object getAdapter(Class required) {
+		if (IContentOutlinePage.class.equals(required)) {
+			if (outlinePage == null) {
+				outlinePage = new GoEditorOutlinePage(getDocumentProvider(), this);
+			}
+			
+			return outlinePage;
+		}
+		
+		return super.getAdapter(required);
+	}
+	
+	@Override
+	protected final void doSetInput(IEditorInput input) throws CoreException {
+		super.doSetInput(input);
+		
+		if (input instanceof IFileEditorInput) {
+			imageUpdater = new EditorImageUpdater(this);
+		}
+	}
+	
+	@Override
+	protected void configureSourceViewerDecorationSupport(SourceViewerDecorationSupport support) {
+		super.configureSourceViewerDecorationSupport(support);
+		
+		char[] matchChars = {'(', ')', '[', ']', '{', '}'}; //which brackets to match
+		matcher = new DefaultCharacterPairMatcher(matchChars, IDocumentExtension3.DEFAULT_PARTITIONING);
+		support.setCharacterPairMatcher(matcher);
+		support.setMatchingCharacterPainterPreferenceKeys(
+			GoUIPreferenceConstants.EDITOR_MATCHING_BRACKETS,
+			GoUIPreferenceConstants.EDITOR_MATCHING_BRACKETS_COLOR);
+	}
+	
+	@Override
+	protected void createActions() {
+		super.createActions();
+		
+		IAction action;
+		
+		action = new ToggleCommentAction(editorResourceBundle, "ToggleComment.", this);
+		action.setActionDefinitionId("com.googlecode.goclipse.actions.ToggleComment");
+		setAction("ToggleComment", action);
+		markAsStateDependentAction("ToggleComment", true);
+		configureToggleCommentAction();
+	}
+	
+	public DefaultCharacterPairMatcher getPairMatcher() {
+		return matcher;
+	}
+	
+	public String getText() {
+		return getSourceViewer().getDocument().get();
+	}
+	
+	public void replaceText(String newText) {
+		ISelection sel = getSelectionProvider().getSelection();
+		int topIndex = getSourceViewer().getTopIndex();
+		
+		getSourceViewer().getDocument().set(newText);
+		
+		if (sel != null) {
+			getSelectionProvider().setSelection(sel);
+		}
+		
+		if (topIndex != -1) {
+			getSourceViewer().setTopIndex(topIndex);
+		}
+	}
+	
+	@Override
+	public void dispose() {
+		
+		if (imageUpdater != null) {
+			imageUpdater.dispose();
+		}
+		
+		super.dispose();
+	}
+	
+	private void configureToggleCommentAction() {
+		IAction action = getAction("ToggleComment");
+		
+		if (action instanceof ToggleCommentAction) {
+			ISourceViewer sourceViewer = getSourceViewer();
+			SourceViewerConfiguration configuration = getSourceViewerConfiguration();
+			((ToggleCommentAction) action).configure(sourceViewer, configuration);
+		}
+	}
+	
+	protected void handleReconcilation(IRegion partition) {
+		if (outlinePage != null) {
+			outlinePage.handleEditorReconcilation();
+		}
+	}
+	
 	public IProject getCurrentProject() {
 		if (getEditorInput() instanceof IFileEditorInput) {
 			IFileEditorInput input = (IFileEditorInput) getEditorInput();
-
+			
 			IFile file = input.getFile();
-
+			
 			if (file != null) {
 				return file.getProject();
 			}
 		}
-
+		
 		return null;
 	}
-
-  @Override
-  protected boolean isTabsToSpacesConversionEnabled() {
-    return false;
-  }
-
+	
+	@Override
+	protected boolean isTabsToSpacesConversionEnabled() {
+		return false;
+	}
+	
 }
