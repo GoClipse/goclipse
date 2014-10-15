146141149de7e88c71bbc3ffd8a546344223d313
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-10-15 18:08:00 +0100

    Minor changes.

---
+++ plugin_ide.core/src-lang/melnorme/lang/ide/core/utils/process/IRunProcessTask.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2014 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
+package melnorme.lang.ide.core.utils.process;
+
+import melnorme.utilbox.core.fntypes.ICallable;
+import melnorme.utilbox.process.ExternalProcessHelper.ExternalProcessResult;
+
+import org.eclipse.core.runtime.CoreException;
+
+public interface IRunProcessTask extends ICallable<ExternalProcessResult, CoreException> {
+}
