66f763b73d6d85287fbf2b749d77304bea96011c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-10-16 15:41:54 +0100

    ModuleFullName refactoring. (AbstractElementName)

--- plugin_tooling/src-lang/melnorme/lang/utils/ProcessUtils.java
+++ plugin_tooling/src-lang/melnorme/lang/utils/ProcessUtils.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Bruno Medeiros - initial API and implementation
  *******************************************************************************/
-package melnorme.lang.tooling;
+package melnorme.lang.utils;
 
 import static melnorme.utilbox.core.Assert.AssertNamespace.assertTrue;
 
