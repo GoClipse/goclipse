07434e174d56a787731608e80268f7618d1dad62
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-10-22 12:55:17 +0100

    Rewrote builder clean.

--- plugin_ide.core/src/com/googlecode/goclipse/builder/GoBuilder.java
+++ plugin_ide.core/src/com/googlecode/goclipse/builder/GoBuilder.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2014 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
 package com.googlecode.goclipse.builder;
 
 import java.io.File;
@@ -6,22 +16,17 @@ import java.util.Map;
 
 import melnorme.lang.ide.core.LangCore;
 import melnorme.lang.ide.core.operations.LangProjectBuilder;
-import melnorme.lang.tooling.ops.ToolSourceError;
 import melnorme.utilbox.collections.ArrayList2;
 import melnorme.utilbox.core.CommonException;
 import melnorme.utilbox.misc.MiscUtil;
 import melnorme.utilbox.process.ExternalProcessHelper.ExternalProcessResult;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-import com.googlecode.goclipse.Environment;
-import com.googlecode.goclipse.core.GoCoreMessages;
+import com.googlecode.goclipse.core.GoCore;
 import com.googlecode.goclipse.core.GoEnvironmentPrefs;
 import com.googlecode.goclipse.core.GoProjectEnvironment;
 import com.googlecode.goclipse.core.GoProjectPrefConstants;
@@ -32,7 +37,7 @@ import com.googlecode.goclipse.tooling.env.GoEnvironment;
 
 public class GoBuilder extends LangProjectBuilder {
 	
-	public  static final String  BUILDER_ID = "com.googlecode.goclipse.goBuilder";
+	public static final String BUILDER_ID = "com.googlecode.goclipse.goBuilder";
 	
 	public GoBuilder() {
 	}
@@ -47,25 +52,10 @@ public class GoBuilder extends LangProjectBuilder {
 		return super.build(kind, args, monitor);
 	}
 	
-	private boolean checkBuild(IProject project) throws CoreException {
-		
-		if (!GoProjectEnvironment.isValid(project)){
-			MarkerUtilities.addMarker(getProject(), GoCoreMessages.INVALID_PREFERENCES_MESSAGE);
-			return false;
-			
-		} else {
-			return true;
-		}
-	}
-	
 	@Override
 	protected IProject[] doBuild(final IProject project, int kind, Map<String, String> args, IProgressMonitor monitor)
 			throws CoreException {
 		
- 		if (!checkBuild(project)) {
-			return null;
-		}
- 		
 		GoToolManager.getDefault().notifyBuildStarting(project);
 		
 		try {
@@ -91,23 +81,15 @@ public class GoBuilder extends LangProjectBuilder {
 	
 	protected void doBuildAll(final IProject project, IProgressMonitor monitor) throws CoreException {
 		
-		GoEnvironment goEnvironment = GoProjectEnvironment.getGoEnvironment(project);
-		String compilerPath = GoEnvironmentPrefs.COMPILER_PATH.get();
+		GoEnvironment goEnv = getValidGoEnvironment(project);
 		
-		if(compilerPath.isEmpty()) {
-			throw LangCore.createCoreException("Compiler Path not defined.", null);
-		}
-		ArrayList2<String> goBuildCmdLine = new ArrayList2<>(compilerPath, "install", "-v");
-		
+		ArrayList2<String> goBuildCmdLine = getGoToolCommandLine();
+		goBuildCmdLine.addElements("install", "-v");
 		goBuildCmdLine.addElements(GoProjectPrefConstants.GO_BUILD_EXTRA_OPTIONS.getParsedArguments(project));
+		addSourcePackagesToCmdLine(project, goBuildCmdLine, goEnv);
 		
-		Collection<GoPackageName> sourcePackages = GoProjectEnvironment.getSourcePackages(project, goEnvironment);
-		for (GoPackageName goPackageName : sourcePackages) {
-			goBuildCmdLine.add(goPackageName.getFullNameAsString());
-		}
-		
-		ExternalProcessResult buildAllResult = GoToolManager.getDefault().runBuildTool(goEnvironment, project, monitor,
-			project.getLocation().toFile(), goBuildCmdLine);
+		ExternalProcessResult buildAllResult = 
+			GoToolManager.getDefault().runBuildTool(goEnv, project, monitor, getProjectLocation(), goBuildCmdLine);
 		
 		GoBuildOutputProcessor buildOutput = new GoBuildOutputProcessor(MiscUtil.createValidPath("")) {
 			@Override
@@ -117,67 +99,50 @@ public class GoBuilder extends LangProjectBuilder {
 		};
 		buildOutput.parseOutput(buildAllResult);
 		
-		ArrayList2<ToolSourceError> buildErrors = buildOutput.getBuildErrors();
-		addErrorMarkers(buildErrors);
+		addErrorMarkers(buildOutput.getBuildErrors());
+	}
+	
+	protected File getProjectLocation() {
+		return getProject().getLocation().toFile();
+	}
+	
+	protected GoEnvironment getValidGoEnvironment(final IProject project) throws CoreException {
+		GoEnvironment goEnv = GoProjectEnvironment.getGoEnvironment(project);
+		if(!goEnv.isValid()) { /* FIXME: one exception per build. */
+			throw GoCore.createCoreException("Go Environment settings are not valid", null);
+		}
+		return goEnv;
+	}
+	
+	protected ArrayList2<String> getGoToolCommandLine() throws CoreException {
+		String compilerPath = GoEnvironmentPrefs.COMPILER_PATH.get();
+		
+		if(compilerPath.isEmpty()) {
+			throw LangCore.createCoreException("Compiler Path not defined.", null);
+		}
+		return new ArrayList2<>(compilerPath);
+	}
+	
+	protected void addSourcePackagesToCmdLine(final IProject project, ArrayList2<String> goBuildCmdLine,
+			GoEnvironment goEnvironment) throws CoreException {
+		Collection<GoPackageName> sourcePackages = GoProjectEnvironment.getSourcePackages(project, goEnvironment);
+		for (GoPackageName goPackageName : sourcePackages) {
+			goBuildCmdLine.add(goPackageName.getFullNameAsString());
+		}
 	}
 	
 	@Override
 	protected void clean(IProgressMonitor monitor) throws CoreException {
+		deleteBuildMarkers();
+		
 		IProject project = getProject();
+		GoEnvironment goEnv = getValidGoEnvironment(project);
 		
-		MarkerUtilities.deleteAllMarkers(project);
+		ArrayList2<String> goBuildCmdLine = getGoToolCommandLine();
+		goBuildCmdLine.addElements("clean");
+		addSourcePackagesToCmdLine(project, goBuildCmdLine, goEnv);
 		
-		File binFolder = GoProjectEnvironment.getBinFolder(project).toFile();
-		if (binFolder.exists()) {
-			deleteFolder(binFolder, true);
-		}
-		
-		File pkgFolder = GoProjectEnvironment.getPkgFolder(project).toFile();
-		if (pkgFolder.exists()) {
-			deleteFolder(pkgFolder, true);
-		}
-		
-		project.accept(new IResourceVisitor() {
-			@Override
-			public boolean visit(IResource resource) throws CoreException {
-				IPath relativePath = resource.getProjectRelativePath();
-				String lastSegment = relativePath.lastSegment();
-				IPath rawLocation = resource.getRawLocation();
-				if (rawLocation != null) {
-					File file = rawLocation.toFile();
-					if (file.exists() && file.isDirectory() 
-							&& (Environment.isPkgFile(relativePath)) 
-							&& (lastSegment.equals(GoConstants.OBJ_FILE_DIRECTORY) || lastSegment.equals(GoConstants.TEST_FILE_DIRECTORY)))
-					{
-						deleteFolder(file, true);
-					}
-				}
-				return resource instanceof IContainer;
-			}
-		}, IResource.DEPTH_INFINITE, false);
-		
-		project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+		GoToolManager.getDefault().runBuildTool(goEnv, project, monitor, getProjectLocation(), goBuildCmdLine);
 	}
-
-	private boolean deleteFolder(File f, boolean justContents) {
-		if (!f.exists()) {
-			return false;
-		}
-		if (f.isDirectory()) {
-			String[] children = f.list();
-			for (int i = 0; i < children.length; i++) {
-				boolean success = deleteFolder(new File(f, children[i]), false);
-				if (!success) {
-					return false;
-				}
-			}
-			if (!justContents) {
-				f.delete();
-			}
-		} else {
-			f.delete();
-		}
-		return true;
-	}
-
+	
 }
