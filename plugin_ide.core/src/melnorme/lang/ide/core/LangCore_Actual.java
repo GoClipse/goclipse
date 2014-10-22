5793a2603a8a16d18f7a8c9fb453b5f7b276a3cc
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-10-22 17:50:16 +0100

    Lang: LangCore_Actual.BUILD_PROBLEM_ID

--- plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
+++ plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
@@ -3,11 +3,14 @@ package melnorme.lang.ide.core;
 import org.eclipse.core.runtime.Plugin;
 
 import com.googlecode.goclipse.Activator;
+import com.googlecode.goclipse.core.GoCore;
 
 public class LangCore_Actual {
 	
 	public static final String PLUGIN_ID = "com.googlecode.goclipse.core";
 	public static final String NATURE_ID = Activator.PLUGIN_ID + ".goNature";
+	
+	public static final String BUILD_PROBLEM_ID = GoCore.PLUGIN_ID + ".goProblem";
 	
 	public static Plugin getInstance() {
 		return Activator.getDefault();
