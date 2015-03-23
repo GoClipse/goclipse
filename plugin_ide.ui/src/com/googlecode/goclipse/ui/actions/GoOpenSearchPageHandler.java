307960d208735db5723e28efb1bc45bfe987673a
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-03-23 18:02:11 +0000

    Cleanup of menu/toolbar/action contributions.

---
+++ plugin_ide.ui/src/com/googlecode/goclipse/ui/actions/GoOpenSearchPageHandler.java
@@ -0,0 +1,34 @@
+package com.googlecode.goclipse.ui.actions;
+/*******************************************************************************
+ * Copyright (c) 2015, 2015 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.googlecode.goclipse.search.GoSearchPage;
+
+
+public class GoOpenSearchPageHandler extends AbstractHandler {
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		
+		NewSearchUI.openSearchDialog(window, GoSearchPage.EXTENSION_POINT_ID);
+		
+		return null;
+	}
+	
+}
