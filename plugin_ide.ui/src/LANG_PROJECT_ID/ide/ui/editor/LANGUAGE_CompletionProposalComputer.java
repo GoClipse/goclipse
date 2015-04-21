fa7a317783634af04b32b9462ef467d4630d7213
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-04-21 20:30:40 +0100

    @follow-up

--- plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/editor/LANGUAGE_CompletionProposalComputer.java
+++ plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/editor/LANGUAGE_CompletionProposalComputer.java
@@ -13,11 +13,14 @@ package LANG_PROJECT_ID.ide.ui.editor;
 import melnorme.lang.ide.core.operations.TimeoutProgressMonitor;
 import melnorme.lang.ide.ui.editor.actions.SourceOperationContext;
 import melnorme.lang.ide.ui.text.completion.LangCompletionProposalComputer;
+import melnorme.lang.ide.ui.views.LangImageProvider;
 import melnorme.lang.tooling.completion.LangCompletionResult;
 import melnorme.utilbox.concurrency.OperationCancellation;
 import melnorme.utilbox.core.CommonException;
 
 import org.eclipse.core.runtime.CoreException;
+
+import LANG_PROJECT_ID.ide.ui.LANGUAGE_ImageProvider;
 
 public class LANGUAGE_CompletionProposalComputer extends LangCompletionProposalComputer {
 	
@@ -25,6 +28,11 @@ public class LANGUAGE_CompletionProposalComputer extends LangCompletionProposalC
 	protected LangCompletionResult doComputeProposals(SourceOperationContext context, int offset,
 			TimeoutProgressMonitor pm) throws CoreException, CommonException, OperationCancellation {
 		return new LangCompletionResult("NOT IMPLEMENTED");
+	}
+	
+	@Override
+	protected LangImageProvider getImageProvider() {
+		return new LANGUAGE_ImageProvider();
 	}
 	
 }
