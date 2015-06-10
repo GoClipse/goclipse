1b9b346abaef629ceac109af7a138451176a2d6a
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-06-10 12:46:33 +0100

    LANG: fix text hover not showing on other content types.

--- plugin_ide.ui/src-lang/melnorme/lang/ide/ui/text/AbstractLangSourceViewerConfiguration.java
+++ plugin_ide.ui/src-lang/melnorme/lang/ide/ui/text/AbstractLangSourceViewerConfiguration.java
@@ -91,10 +91,7 @@ public abstract class AbstractLangSourceViewerConfiguration extends SimpleLangSo
 	
 	@Override
 	public ITextHover getTextHover(ISourceViewer sourceViewer, String contentType, int stateMask) {
-		if(contentType.equals(IDocument.DEFAULT_CONTENT_TYPE)) {
-			return new BestMatchHover(getEditor());
-		}
-		return null;
+		return new BestMatchHover(getEditor());
 	}
 	
 	@Override
