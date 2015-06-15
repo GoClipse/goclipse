7696effcb4882e39ddfa40d620f160f0e323cf0b
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-06-15 18:01:06 +0100

    Removed com.googlecode.goclipse.go.lib.indexer package.

--- plugin_ide.core/src/com/googlecode/goclipse/go/lib/indexer/TokenClass.java
+++
@@ -1,6 +0,0 @@
-package com.googlecode.goclipse.go.lib.indexer;
-
-public enum TokenClass {
-   TYPE, VALUE, BUILT_IN_FUNCTION, KEYWORD
-
-}
--- plugin_ide.core/src/com/googlecode/goclipse/go/lib/indexer/UnresolvedFunc.java
+++
@@ -1,5 +0,0 @@
-package com.googlecode.goclipse.go.lib.indexer;
-
-public class UnresolvedFunc extends Func{
-
-}
