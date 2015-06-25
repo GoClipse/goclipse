f4bf0dcccd67b9f534825e0dcac50d4dd5defe79
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-06-25 15:47:00 +0100

    LANG: joined pref constants into ToolchainPreferences.

--- plugin_ide.core/src-lang/melnorme/lang/ide/core/operations/ToolchainPreferences.java
+++ plugin_ide.core/src-lang/melnorme/lang/ide/core/operations/ToolchainPreferences.java
@@ -13,13 +13,16 @@ package melnorme.lang.ide.core.operations;
 import melnorme.lang.ide.core.utils.prefs.BooleanPreference;
 import melnorme.lang.ide.core.utils.prefs.StringPreference;
 
-public interface DaemonEnginePreferences {
-
-	public static final BooleanPreference AUTO_START_SERVER =
+public interface ToolchainPreferences {
+	
+	public static final StringPreference SDK_PATH = new StringPreference("sdk_path", "");
+	
+	
+	public static final BooleanPreference AUTO_START_DAEMON =
 			new BooleanPreference("auto_start_daemon", true);
 	public static final StringPreference DAEMON_PATH =
 			new StringPreference("daemon_path", "");
 	public static final BooleanPreference DAEMON_CONSOLE_ENABLE =
 			new BooleanPreference("daemon_console_enable", true);
-			
+	
 }
