28180fb8630cbf23c6c078e1a99ca041a30719f4
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-07-07 15:00:36 +0100

    Remove more unused code (com.googlecode.goclipse.go.lang.parser)

--- plugin_ide.core/src/com/googlecode/goclipse/go/lang/parser/FunctionParserListener.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.goclipse.go.lang.parser;
-
-import com.googlecode.goclipse.go.lang.model.Function;
-
-public interface FunctionParserListener {
-	void onEnterFunction(Function function);
-	void onExitFunction(Function function);
-}
