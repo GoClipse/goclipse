5fd3ac7ad36e2edea3f3baf992a3a26e9213e4d7
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-07-07 15:07:35 +0100

    Might as well remove com.googlecode.goclipse.go.lang.lexer too

--- plugin_ide.core/src/com/googlecode/goclipse/go/lang/lexer/LexerListener.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.goclipse.go.lang.lexer;
-
-
-public interface LexerListener {
-
-   void 	tokenFound(TokenType type, String value, boolean inComment, int start, int end);
-   void 	newline(int lineNumber);
-   boolean  isWhitespaceParser();
-}
--- plugin_ide.core/src/com/googlecode/goclipse/go/lang/lexer/TokenUnit.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.goclipse.go.lang.lexer;
-
-public class TokenUnit{
-	public TokenType tokenType;
-	public String    text;
-	public int 		 start;
-	public int 		 end;
-}
