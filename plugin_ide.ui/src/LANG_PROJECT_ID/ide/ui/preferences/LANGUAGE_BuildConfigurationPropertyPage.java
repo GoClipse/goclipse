6aeecb606e1a85eb318d8ee64103ad9c40ce7e84
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-07-22 19:03:47 +0100

    @follow up

---
+++ plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/preferences/LANGUAGE_BuildConfigurationPropertyPage.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
+package LANG_PROJECT_ID.ide.ui.preferences;
+
+import org.eclipse.core.resources.IProject;
+
+import melnorme.lang.ide.ui.dialogs.AbstractProjectPropertyPage;
+import melnorme.lang.ide.ui.preferences.LangProjectOptionsBlock;
+
+
+public class LANGUAGE_BuildConfigurationPropertyPage extends AbstractProjectPropertyPage {
+	
+	@Override
+	protected LangProjectOptionsBlock createProjectOptionsComponent(IProject project) {
+		return new LangProjectOptionsBlock(project) {
+			@Override
+			protected String init_getArgumentsField_Label() {
+				return "Build Target options:";
+			}
+		};
+	}
+	
+}
