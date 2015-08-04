e9bf247025c115d1571c471b4410c8ba39e58917
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-08-04 12:34:49 +0100

    @follow up

--- plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/navigator/LANGUAGE_NavigatorActionProvider.java
+++ plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/navigator/LANGUAGE_NavigatorActionProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2014 IBM Corporation and other contributors.
+ * Copyright (c) 2015 Bruno Medeiros and other Contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,23 @@
  *******************************************************************************/
 package LANG_PROJECT_ID.ide.ui.navigator;
 
+import org.eclipse.ui.IViewPart;
+
+import LANG_PROJECT_ID.ide.ui.launch.LANGUAGE_LaunchShortcut;
+import melnorme.lang.ide.ui.launch.LangLaunchShortcut;
+import melnorme.lang.ide.ui.navigator.BuildTargetsActionGroup;
 import melnorme.lang.ide.ui.navigator.LangNavigatorActionProvider;
 
 public class LANGUAGE_NavigatorActionProvider extends LangNavigatorActionProvider {
+	
+	@Override
+	protected BuildTargetsActionGroup createBuildTargetsActionGroup(IViewPart viewPart) {
+		return new BuildTargetsActionGroup(viewPart) {
+			@Override
+			protected LangLaunchShortcut createLaunchShortcut() {
+				return new LANGUAGE_LaunchShortcut();
+			}
+		};
+	}
 	
 }
