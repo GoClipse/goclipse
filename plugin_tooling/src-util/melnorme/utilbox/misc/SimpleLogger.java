08b762d6509bd85d6a47bd787f8936b66c1f5989
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-09-25 17:39:53 +0100

    LANG: simplify copyright header.

--- plugin_tooling/src-util/melnorme/utilbox/misc/SimpleLogger.java
+++ plugin_tooling/src-util/melnorme/utilbox/misc/SimpleLogger.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2014 Bruno Medeiros and other Contributors.
+ * Copyright (c) 2007 Bruno Medeiros and other Contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
