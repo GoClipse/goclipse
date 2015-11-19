3b3fcb06e15fb6380cf4a96c8e56d9f02589ccb2
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-11-19 22:46:56 +0000

    Refactor LangCore_Actual

--- plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
+++ plugin_ide.core/src/melnorme/lang/ide/core/LangCore_Actual.java
@@ -4,6 +4,7 @@ import LANG_PROJECT_ID.ide.core.bundle_model.LANGUAGE_BundleModelManager;
 import LANG_PROJECT_ID.ide.core.engine.LANGUAGE_SourceModelManager;
 import LANG_PROJECT_ID.ide.core.operations.LANGUAGE_BuildManager;
 import LANG_PROJECT_ID.ide.core.operations.LANGUAGE_ToolManager;
+import melnorme.lang.ide.core.operations.AbstractToolManager;
 import melnorme.lang.ide.core.operations.build.BuildManager;
 import melnorme.lang.ide.core.project_model.LangBundleModel;
 
@@ -18,25 +19,47 @@ public class LangCore_Actual {
 	
 	public static final String LANGUAGE_NAME = "Lang";
 	
+	
+	public static LangCore2 instance;
+	
+	/* ----------------- Owned singletons: ----------------- */
+	
+	protected final AbstractToolManager toolManager;
+	protected final LANGUAGE_BundleModelManager bundleManager;
+	protected final BuildManager buildManager;
+	protected final LANGUAGE_SourceModelManager sourceModelManager;
+	
+	public LangCore_Actual() {
+		instance = (LangCore2) this;
+		LangCore.pluginInstance.langCore = instance;
+		
+		toolManager = createToolManagerSingleton();
+		bundleManager = createBundleModelManager();
+		buildManager = createBuildManager(bundleManager.getModel());
+		sourceModelManager = createSourceModelManager();
+	}
+	
 	public static LANGUAGE_ToolManager createToolManagerSingleton() {
 		return new LANGUAGE_ToolManager();
+	}
+	
+	public static LANGUAGE_BundleModelManager createBundleModelManager() {
+		return new LANGUAGE_BundleModelManager();
+	}
+	public static BuildManager createBuildManager(LangBundleModel bundleModel) {
+		return new LANGUAGE_BuildManager(bundleModel);
 	}
 	
 	public static LANGUAGE_SourceModelManager createSourceModelManager() {
 		return new LANGUAGE_SourceModelManager();
 	}
 	
-	public static LANGUAGE_BundleModelManager createBundleModelManager() {
-		return new LANGUAGE_BundleModelManager();
-	}
+	
+	/* -----------------  ----------------- */
+	
 	public static LANGUAGE_BundleModel getBundleModel() {
-		return (LANGUAGE_BundleModel) LangCore.getBundleModel();
+		return instance.bundleManager.getModel();
 	}
-	public static BuildManager createBuildManager() {
-		return new LANGUAGE_BuildManager(getBundleModel());
-	}
-	
-	
 	public static class LANGUAGE_BundleModel extends LangBundleModel {
 		
 	}
