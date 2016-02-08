ae15cdbb243483a0f5edd733925fc3e3b4ed4475
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-02-08 18:33:52 +0000

    Correct package and src-folder

--- plugin_ide.core/src-lang/melnorme/lang/ide/core/engine/AutoSaveManager.java
+++ plugin_ide.core/src-lang/melnorme/lang/ide/core/engine/AutoSaveManager.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Bruno Medeiros - initial API and implementation
  *******************************************************************************/
-package mmrnmhrm.core.engine;
+package melnorme.lang.ide.core.engine;
 
 import static melnorme.utilbox.core.Assert.AssertNamespace.assertNotNull;
 
