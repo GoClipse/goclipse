d0271b205c2110fa1e76268ae2f5c6f61b4695f9
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-02-23 19:40:36 +0000

    Refactor GoSDKConfigBlock

--- plugin_ide.ui/src/com/googlecode/goclipse/ui/preferences/GoSDKConfigBlock.java
+++ plugin_ide.ui/src/com/googlecode/goclipse/ui/preferences/GoSDKConfigBlock.java
@@ -12,19 +12,14 @@ package com.googlecode.goclipse.ui.preferences;
 
 import java.io.File;
 
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 
 import com.googlecode.goclipse.core.GoEnvironmentPrefs;
 import com.googlecode.goclipse.tooling.GoSDKLocationValidator;
 
-import melnorme.lang.ide.ui.preferences.AbstractCompositePreferencesBlock;
-import melnorme.lang.ide.ui.preferences.common.AbstractPreferencesBlock2;
+import melnorme.lang.ide.ui.preferences.LangSDKConfigBlock;
 import melnorme.lang.ide.ui.preferences.common.PreferencesPageContext;
-import melnorme.lang.tooling.data.IValidatableValue.ValidatableField;
+import melnorme.lang.tooling.ops.util.PathValidator;
 import melnorme.lang.utils.EnvUtils;
 import melnorme.util.swt.components.fields.CheckBoxField;
 import melnorme.util.swt.components.fields.DirectoryTextField;
@@ -32,73 +27,58 @@ import melnorme.util.swt.components.fields.EnablementButtonTextField;
 import melnorme.utilbox.concurrency.OperationCancellation;
 import melnorme.utilbox.core.CommonException;
 
-public class GoSDKConfigBlock extends AbstractCompositePreferencesBlock {
+public class GoSDKConfigBlock extends LangSDKConfigBlock {
 	
-	public final DirectoryTextField goRootField = new DirectoryTextField("Directory:");
-	protected final GoSDKLocationValidator goSDKLocationValidator = new GoSDKLocationValidator();
-	public final ValidatableField<String> validatedGoRoot = new ValidatableField<>(goRootField, goSDKLocationValidator);
-	
-	protected final EnablementButtonTextField goPathField = new GoPathField();
-	protected final CheckBoxField gopathAppendProjectLocField = new CheckBoxField(
-		"Also add project location to GOPATH, if it's not contained there already.");
+	protected final GoPathGroup goPathGroup = new GoPathGroup();
 	
 	public GoSDKConfigBlock(PreferencesPageContext prefContext) {
 		super(prefContext);
 		
-		addSubComponent(goRootField);
-		
-		addSubComponent(goPathField);
-		addSubComponent(gopathAppendProjectLocField);
-		
-		prefContext.bindToPreference(goRootField, GoEnvironmentPrefs.GO_ROOT);
-		
-		prefContext.bindToPreference(goPathField.asEffectiveValueProperty2(), GoEnvironmentPrefs.GO_PATH);
-		prefContext.bindToPreference(gopathAppendProjectLocField, GoEnvironmentPrefs.APPEND_PROJECT_LOC_TO_GOPATH);
-		
-		
-		validation.addFieldValidation(true, goRootField, goSDKLocationValidator);
+		addSubComponent(goPathGroup);
 	}
 	
+	@Override
+	protected PathValidator getSDKValidator() {
+		return new GoSDKLocationValidator();
+	}
+	
+	@Override
+	protected LanguageSDKLocationGroup init_createSDKLocationGroup() {
+		return new LanguageSDKLocationGroup() {
+			@Override
+			protected void initBindings() {
+				prefContext.bindToPreference(sdkLocationField, GoEnvironmentPrefs.GO_ROOT);
+				validation.addFieldValidation(true, sdkLocationField, getSDKValidator());
+			}
+		};
+	}
+	
+
 	/* -----------------  ----------------- */
 	
-	@Override
-	public int getPreferredLayoutColumns() {
-		return 1;
-	}
-	
-	@Override
-	protected void createContents(Composite topControl) {
-		// Ignore super
-		//super.createContents(topControl);
+	public class GoPathGroup extends EnablementButtonTextField {
 		
-		Group goSDK = AbstractPreferencesBlock2.createOptionsSection(topControl, "Go installation (GOROOT):", 3,
-			createSubComponentDefaultGridData());
+		protected final CheckBoxField gopathAppendProjectLocField = new CheckBoxField(
+				"Also add project location to GOPATH, if it's not contained there already.");
 		
-		goRootField.createComponentInlined(goSDK);
+		public GoPathGroup() {
+			super("Eclipse GOPATH:", "Use same value as the GOPATH environment variable.", "Add Folder");
+			
+			prefContext.bindToPreference(asEffectiveValueProperty2(), GoEnvironmentPrefs.GO_PATH);
+			
+			prefContext.bindToPreference(gopathAppendProjectLocField, GoEnvironmentPrefs.APPEND_PROJECT_LOC_TO_GOPATH);
+		}
 		
-		/* -----------------  ----------------- */
+		@Override
+		protected void createContents_all(Composite topControl) {
+			super.createContents_all(topControl);
+			gopathAppendProjectLocField.createComponentInlined(topControl);
+		}
 		
-		Composite goPathFieldTopControl = goPathField.createComponent(topControl, 
-			createSubComponentDefaultGridData());
-		gopathAppendProjectLocField.createComponent(goPathFieldTopControl);
-		
-		goRootField.addListener(() -> handleGoRootChange());
-	}
-	
-	@Override
-	protected GridData createSubComponentDefaultGridData() {
-		return GridDataFactory.fillDefaults().grab(true, false).minSize(300, SWT.DEFAULT).create();
-	}
-	
-	protected void handleGoRootChange() {
-	}
-	
-	/* -----------------  ----------------- */
-	
-	public static class GoPathField extends EnablementButtonTextField {
-		
-		public GoPathField() {
-			super("Eclipse GOPATH:", "Use same as the GOPATH environment variable.", "Add Folder");
+		@Override
+		protected void doSetEnabled(boolean enabled) {
+			super.doSetEnabled(enabled);
+			gopathAppendProjectLocField.setEnabled(enabled);
 		}
 		
 		@Override
