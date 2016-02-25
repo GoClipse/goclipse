15b0c0776dfa3853113bd512f46330233019d8a0
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-02-25 14:24:42 +0000

    addendum

--- plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/preferences/LANGUAGE_DaemonPreferencePage.java
+++ plugin_ide.ui/src/LANG_PROJECT_ID/ide/ui/preferences/LANGUAGE_DaemonPreferencePage.java
@@ -1,21 +1,15 @@
 package LANG_PROJECT_ID.ide.ui.preferences;
 
 
-import melnorme.lang.ide.ui.LangUIPlugin_Actual;
-import melnorme.lang.ide.ui.preferences.common.AbstractPreferencesBlock2;
 import melnorme.lang.ide.ui.preferences.common.PreferencesPageContext;
-import melnorme.lang.ide.ui.preferences.pages.DaemonToolPreferencePage;
+import melnorme.lang.ide.ui.preferences.pages.EngineToolsPreferencePage;
+import melnorme.lang.ide.ui.preferences.pages.LanguageToolsBlock;
 
-public class LANGUAGE_DaemonPreferencePage extends DaemonToolPreferencePage {
+public class LANGUAGE_DaemonPreferencePage extends EngineToolsPreferencePage {
 	
 	@Override
-	protected AbstractPreferencesBlock2 init_createPreferencesBlock(PreferencesPageContext prefContext) {
-		return new ServerToolsBlock(prefContext) {
-			@Override
-			protected String getDaemonToolName() {
-				return LangUIPlugin_Actual.DAEMON_TOOL_Name;
-			}
-		};
+	protected LanguageToolsBlock init_createPreferencesBlock(PreferencesPageContext prefContext) {
+		return new LanguageToolsBlock(prefContext) { };
 	}
 	
 }
